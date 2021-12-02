3cd24652a191cc381b23bc01a0aae5acd9f4fef0
Paolo Martini <mrtnpaolo@protonmail.com>
2021-12-02 06:24:58 +0100

    🎄 day-02: part 1 & 2

---
+++ execs/Day02.hs
@@ -0,0 +1,36 @@
+module Main (main) where
+
+import Advent
+
+import Data.List (foldl')
+
+data Movement = Aim Int | Move Int
+  deriving (Show)
+
+main =
+  do inp <- getInputLines words 2
+     print (part1 (concatMap toCoord inp))
+     print (part2 (map toMove inp))
+  where
+    toCoord ["forward",read -> n] = replicate n east
+    toCoord ["up"     ,read -> n] = replicate n north
+    toCoord ["down"   ,read -> n] = replicate n south
+
+    toMove  ["forward",read -> n] = Move n
+    toMove  ["up"     ,read -> n] = Aim (negate n)
+    toMove  ["down"   ,read -> n] = Aim n
+
+part1 xs = endy*endx
+  where
+    C endy endx = foldl' addCoord origin xs
+
+part2 xs = endy*endx
+  where
+    (C endy endx,_) = foldl' step (origin,0) xs
+
+    step (c    ,aim) (Aim  m) = (c      ,aim+m)
+
+    step (C y x,aim) (Move n) = (C y' x',aim  )
+      where
+        y' = y + n*aim
+        x' = x + n
