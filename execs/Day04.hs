7cf7e092becb4ce220227ee9354854c07fe6d177
Paolo Martini <mrtnpaolo@protonmail.com>
2021-12-04 14:37:00 +0100

    👍 day-04: remove arrays and use simple lists

--- execs/Day04.hs
+++ execs/Day04.hs
@@ -1,47 +1,39 @@
 module Main (main) where
 
-import Advent (getInput)
-
-import Data.Ix   (range)
-import Data.List (inits,unfoldr,partition)
-
-import Data.IntSet        qualified as IS (fromList,member)
-import Data.Array.Unboxed qualified as A  (UArray,listArray,elems,(!))
-
-type Board = A.UArray (Int,Int) Int
+import Advent    (getInput)
+import Data.List (unfoldr,partition,transpose)
 
 main =
-  do (turns,boards) <- getInput (parse . lines) 4
-     let winners = play turns boards
+  do (balls,boards) <- getInput (parse . lines) 4
+     let winners = play balls boards
      print (part1 winners)
      print (part2 winners)
   where
-    parse (xs:_:ys) = (turns,boards)
+    parse (xs:_:ys) = (balls,boards)
       where
-        turns = inits (read @[Int] ('[' : xs ++ "]"))
+        balls = read @[Int] ('[' : xs ++ "]")
 
         boards = unfoldr collect ys
-
-        collect [] = Nothing
-        collect xs = Just (board,drop 6 xs)
           where
-            board = A.listArray ((0,0),(4,4)) entries :: Board
-            entries = concatMap (map (read @Int) . words) (take 5 xs)
+            collect [] = Nothing
+            collect xs = Just (board,drop 6 xs)
+              where
+                board = map (map (read @Int) . words) (take 5 xs)
 
-play [] _ = []
-play (draws:nexts) boards = map (score draws) winners ++ play nexts rest
+play []     _      = []
+play (n:ns) boards = map (score n) winners ++ play ns rest
   where
-    (winners,rest) = partition (winning draws) boards
+    (winners,rest) = partition winning (map (mark n) boards)
 
-winning :: [Int] -> Board -> Bool
-winning (IS.fromList -> drawn) board =
-  or [ all (`IS.member` drawn) [ board A.! c | c <- cs ]
-     | cs <- rows ++ cols ]
+mark n = map (map replace)
   where
-    rows = [ [ (i,j) | j <- [0..4] ] | i <- [0..4] ]
-    cols = [ [ (i,j) | i <- [0..4] ] | j <- [0..4] ]
+    replace x | x == n = -1 | otherwise = x
 
-score draws b = last draws * sum [ n | n <- A.elems b, n `notElem` draws ]
+winning board = bingo board || bingo (transpose board)
+  where
+    bingo = any (all (-1 ==))
+
+score ball b = ball * sum [ n | n <- concat b, n /= -1 ]
 
 part1 = head
 
