2fcee9d1ad62aba049788e6e3068afc098667387
Paolo Martini <mrtnpaolo@protonmail.com>
2021-12-07 05:50:58 +0100

    👷 add Advent.List.freqs

--- common/Advent/List.hs
+++ common/Advent/List.hs
@@ -1,10 +1,17 @@
 module Advent.List
   ( count
+  , freqs
   ) where
 
 import Data.List (foldl')
+import Data.Map.Strict qualified as M (toAscList,fromListWith)
 
 count :: (a -> Bool) -> [a] -> Int
 count p xs = foldl' f 0 xs
   where
     f n x | p x = n+1 | otherwise = n
+
+freqs :: (Ord a) => [a] -> [(a,Int)]
+freqs xs = combine [ (x,1) | x <- xs ]
+  where
+    combine = M.toAscList . M.fromListWith (+)
