f3d2cab4398eeed67eece0b41b5c534626f7420c
Paolo Martini <mrtnpaolo@protonmail.com>
2021-12-09 10:48:18 +0100

    🏎 day-09: faster still

--- execs/Day09.hs
+++ execs/Day09.hs
@@ -1,27 +1,37 @@
 module Main (main) where
 
-import Advent          (getInput)
-import Advent.Coord    (withCoords,cardinal)
-import Advent.Search   (bfs)
-import Data.Ord        (comparing)
-import Data.Maybe      (maybeToList)
-import Data.List       qualified as L
-import Data.Map.Strict qualified as M
+import Advent             (getInput)
+import Advent.Coord       (withCoords,boundingBox,cardinal,origin,Coord(..),coordCol,coordRow)
+import Advent.Search      (bfs)
+import Data.Ix            (inRange)
+import Data.Ord           (comparing)
+import Data.Maybe         (maybeToList)
+import Data.List          qualified as L
+import Data.Array.Unboxed qualified as A
 
 main =
   do inp <- getInput parse 9
      print (part1 inp)
      print (part2 inp)
   where
-    parse = toMap . withCoords (read @Int . pure) . lines
-    toMap = M.filter (< 9) . M.fromList
+    parse = toArray . withCoords (read @Int . pure) . lines
+    toArray :: [(Coord,Int)] -> A.Array Coord Int
+    toArray ps = A.array bounds ps :: A.Array Coord Int
+      where
+        Just bounds = boundingBox (map fst ps)
+
+cave `at` c
+  | c `inside` cave = pure (cave A.! c)
+  | otherwise       = mempty
+  where
+    c `inside` cave = inRange (A.bounds cave) c
 
 cave `around` c =
-  [ (c',y) | c' <- cardinal c, y <- maybeToList (cave M.!? c') ]
+  [ (c',y) | c' <- cardinal c,  y <- cave `at` c' ]
 
-lows cave = M.toList (M.filterWithKey low cave)
+lows cave = filter low (A.assocs cave)
   where
-    low c x = and [ y > x | (_,y) <- cave `around` c ]
+    low (c,x) = and [ y > x | (_,y) <- cave `around` c ]
 
 part1 cave = sum [ x + 1 | (_,x) <- lows cave ]
 
