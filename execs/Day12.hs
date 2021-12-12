264121fbdc6e11704f80987120dc010f63939ee0
Paolo Martini <mrtnpaolo@protonmail.com>
2021-12-12 07:51:37 +0100

    🧼 day-12: some cleanup

--- execs/Day12.hs
+++ execs/Day12.hs
@@ -1,111 +1,52 @@
 module Main (main) where
 
-import Advent
-import Numeric
-import Data.Ix
-import Data.Ord
-import Data.Char
-import Data.List          qualified as L
-import Data.Set           qualified as S
-import Data.Map.Strict    qualified as M
-import Data.IntSet        qualified as IS
-import Data.IntMap.Strict qualified as IM
-import Data.Array.Unboxed qualified as A
-import Debug.Trace
+import Advent          (getInputLines,count,bfs)
+import Data.Char       (isLower,isUpper)
+import Data.Map.Strict qualified as M
 
 main =
-  do inp <- getInputLines parse 12
-     --mapM_ print $ M.toList (part1 inp
-     let asgiven  = map (\[a,b] -> (a,[b])) inp
-         opposite = map (\[a,b] -> (b,[a])) inp
-         fixend = map \case ("end",_) -> ("end",[]); x -> x
-         links = M.fromListWith (++) (fixend (asgiven ++ opposite))
+  do links <- toLinks <$> getInputLines parse 12
      mapM_ print (M.toList links)
      let paths = part1 links
+     --mapM_ print paths
      print (length paths)
-     mapM_ print (part2 links)
+     let paths2 = part2 links
+     --mapM_ print paths2
+     print (length paths2)
   where
-    parse = words . map sep
+    parse = words . map \case '-' -> ' '; x -> x
+    toLinks inp = M.fromListWith (++) (fixends (asgiven ++ opposite))
       where
-        sep '-' = ' '
-        sep  x  =  x
+        asgiven  = map (\[a,b] -> (a,[b])) inp
+        opposite = map (\[a,b] -> (b,[a])) inp
+        fixends  = map \case ("end",_) -> ("end",[]); x -> x
+                 . map \(from,to) -> (from,filter ("start" /=) to)
 
-part1 links = map reverse toend
+explore pick links = paths
   where
-    toend = filter (\(x:_) -> x == "end") $ filter (not . null) paths
+    paths = filter complete (bfs next ["start"])
 
-    paths = map (\(_,_,path) -> path) $ bfsOn repr next [start]
+    complete ("end":_) = True
+    complete _         = False
 
-    start = ("start",S.singleton "start",[])
+    next path@(here:_) = concatMap (pick links path) (links M.! here)
 
-    next ("end",s,("end":_)) = []
-    next ("end",s,path) = [ ("end",s,"end":path) ]
-    next (here,seenSmall,path) =
-      concatMap pick (links M.! here)
-      where
-        pick there@(t:_)
-          | isLower t && there `S.notMember` seenSmall =
-            [ (there,S.insert there seenSmall,here:path) ]
-          | isUpper t =
-            [ (there,seenSmall,here:path) ]
-          | otherwise =
-            []
+part1 = explore pick
 
-    repr (here,seen,path) = (here,seen,path)
+pick links path there@(t:_)
+  | isLower t && there `notElem` path = [there : path]
+  | isUpper t                         = [there : path]
+  | otherwise                         = []
 
-part2 links = (:[]) . length $ map reverse toend -- length $ filter rule $ map (filter small) $ map reverse toend
-  where
+part2 = explore pick'
 
-    rule xs
-      | Just xs <- counts M.!? 2 = length xs == 1
-      | otherwise = False
-      where
-        counts = M.fromListWith (++) [ (1,[x]) | x <- xs ]
+pick' links path there
+  | or [ count (x ==) path == 2 | x <- filter (isLower . head) path ]
+  = pick links path there
+  | otherwise
+  = pick2 links path there
 
-    small "start" = False
-    small "end" = False
-    small (x:_) = isLower x
-
-    toend = filter (\(x:_) -> x == "end") $ filter (not . null) paths
-
-    paths = map (\(_,_,path) -> path) $ bfsOn repr next [start]
-
-    start = ("start",M.singleton "start" (-1),[])
-
-    next ("end",s,("end":_)) = []
-
-    next ("end",s,path) = [ ("end",s,"end":path) ]
-
-    next (here,seenSmall,path) =
-      concatMap pick (links M.! here L.\\ ["start"])
-
-      where
-        pick there
-          | 2 `elem` M.elems seenSmall = picksingles there
-          | otherwise                  = allowonedouble there
-
-        picksingles there@(t:_)
-          | isLower t && there `M.notMember` seenSmall =
-            [ (there,M.insert there 1 seenSmall,here:path) ]
-
-          | isUpper t =
-            [ (there,seenSmall,here:path) ]
-
-          | otherwise =
-            []
-
-        allowonedouble there@(t:_)
-          | isLower t
-          , Just times <- seenSmall M.!? there
-          , times == 1 =
-            [ (there,M.insert there 2 seenSmall,here:path) ]
-
-          | isLower t =
-            [ (there,M.insert there 1 seenSmall,here:path) ]
-
-          | isUpper t =
-            [ (there,seenSmall,here:path) ]
-
-          | otherwise = []
-
-    repr (here,seen,path) = (here,seen,path)
+pick2 links path there@(t:_)
+  | isLower t && count (there ==) path <= 1 = [there : path]
+  | isUpper t = [there : path]
+  | otherwise = []
