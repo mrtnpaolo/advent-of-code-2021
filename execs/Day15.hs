64a80d058931c58a6b3f1be12d23c9e21b47b010
Paolo Martini <mrtnpaolo@protonmail.com>
2021-12-15 09:39:54 +0100

    🏎 day-15: faster

--- execs/Day15.hs
+++ execs/Day15.hs
@@ -1,38 +1,45 @@
 module Main (main) where
 
-import Advent          (getInput)
-import Advent.Coord    (Coord(..),origin,addCoord,withCoords,cardinal)
-import Advent.Search   (astar)
-import Data.Char       (digitToInt)
-import Data.Maybe      (maybeToList)
-import Data.List       qualified as L
-import Data.Map.Strict qualified as M
+import Advent             (getInputArray)
+import Advent.Coord       (Coord(..),origin,addCoord,withCoords,cardinal)
+import Advent.Search      (astar)
+import Data.Ix            (inRange)
+import Data.Char          (digitToInt)
+import Data.Maybe         (maybeToList)
+import Data.List          qualified as L
+import Data.Array.Unboxed qualified as A
 
 main =
-  do inp <- getInput parse 15
+  do inp <- A.amap digitToInt <$> getInputArray 15
      print (part1 inp)
      print (part2 inp)
-  where
-    parse = M.fromList . withCoords digitToInt . lines
 
 part1 = solve
 
 part2 = solve . stitch
 
-solve m = least
+type T = A.UArray Coord Int
+
+solve :: T -> Int
+solve a = least
   where
-    Just least = L.lookup end search where end = maximum (M.keys m)
+    Just least = L.lookup end search where end = maximum (A.indices a)
 
     search = astar next origin
 
-    next c = [ (d,cost,0) | d <- cardinal c, cost <- maybeToList (m M.!? d) ]
+    next c = [ (d,cost,0) | d <- cardinal c, inside d, let cost = a A.! d ]
 
-stitch m = M.unions [ translate dy dx m | dy <- [0..4], dx <- [0..4] ]
+    inside = inRange (A.bounds a)
 
-translate dy dx m = M.map risk (M.mapKeys move m)
+stitch :: T -> T
+stitch a = A.array (origin,C (5*h - 1) (5*w - 1)) assocs
   where
-    C h w = maximum (M.keys m) `addCoord` C 1 1
+    C h w = let (_,end) = A.bounds a in end `addCoord` C 1 1
 
-    move (C y x) = C (y + dy*h) (x + dx*w)
+    assocs = concat [ map (translate dy dx) (A.assocs a)
+                    | dy <- [0..4], dx <- [0..4] ]
 
-    risk r = cycle [1..9] !! ((r - 1) + dx + dy)
+    translate dy dx (c,r) = (c',r')
+      where
+        c' = c `addCoord` C (dy * h) (dx * w)
+        r' = cycle [1..9] !! (r - 1 + dx + dy)
